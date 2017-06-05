60d35d14fa365e2f13e47db58514e59d3ce5d842
Jordan Benner <jordanbenner@outlook.com>
2017-06-05 09:37:22 -0500

    Idk

--- pg-excersise.js
+++ pg-excersise.js
@@ -9,9 +9,12 @@ db.query('SELECT * FROM restaurant')
   .then(function (results)) {
     results.forEach(function (r) {
       console.log(r.id, r.name. r.address, r.category);
-    });ls
+    });
+    db.one('SELECT* FROM where name="R1"');
 
-
+  })
+  .then(funtion (row){
+    consol.log(row);
   })
   catch(function(error){
     console.error(error);
