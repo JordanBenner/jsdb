7b93ac6a4785e8b795bb661105d95c8249971eee
Jordan Benner <jordanbenner@outlook.com>
2017-06-05 09:29:50 -0500

    Bluebird

--- pg-excersise.js
+++ pg-excersise.js
@@ -1,3 +1,20 @@
-var pgp = require('pg-promise')({});
+var pgp = require('bluebird');
+var pgp = require('pg-promise')({
+  promiseLib: promise
+});
+// var db = pgp({database: 'jsdb'});
 
-var db = pgp({database: 'jsdb'});
+db.query('SELECT * FROM restaurant')
+  .then(function (results)) {
+    results.forEach(function (r) {
+      console.log(r.id, r.name. r.address, r.category);
+    });ls
+
+
+  })
+  catch(function(error){
+    console.error(error);
+  })
+.finally(functioh(){
+  pgp.end();
+});
